56e76fcba42101c31aedc59727b93631a3a70d8e
Fabien-Scetbon <fscetbon@gmail.com>
2024-03-08 18:54:11 +0100

    login

--- woost/app/Livewire/Auth/Login.php
+++ woost/app/Livewire/Auth/Login.php
@@ -3,11 +3,37 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Hash;
+use App\User;
 
 class Login extends Component
 {
+    public $users, $email, $password, $name;
+    public $registerForm = false;
+
     public function render()
     {
-        return view('livewire.auth.login');
+        return view('livewire.login-register');
+    }
+
+    private function resetInputFields(){
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+    }
+
+    public function login()
+    {
+        $validatedDate = $this->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        
+        if(\Auth::attempt(array('email' => $this->email, 'password' => $this->password))){
+                session()->flash('message', "You are Login successful.");
+        }else{
+            session()->flash('error', 'email and password are wrong.');
+        }
     }
 }
